9aa4f17997fbe06b1bbceca8d1c72a9c3950e11c
Alexander Senier <alex@srlabs.de>
2014-10-07 18:38:25 +0200

    Add C3 criterion

---
+++ sql/c_03.sql
@@ -0,0 +1,13 @@
+DROP VIEW IF EXISTS c3;
+CREATE VIEW c3 AS
+SELECT
+        id,
+        timestamp,
+        mcc,
+        mnc,
+        lac,
+        (CASE WHEN cmc_imeisv > 0 THEN 0 ELSE 0.57 END) as score
+FROM session_info
+WHERE cipher > 0;
+
+select * from c3;
