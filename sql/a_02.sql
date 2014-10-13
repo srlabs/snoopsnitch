94635a82fffc44f26a00316d4e2f1b647cca7348
Alexander Senier <alex@srlabs.de>
2014-10-13 19:55:49 +0200

    Base A2 criterion on cell_info
    FIXME: We need to cope with outdated neighboring cell info  using
    timestamps

--- sql/a_02.sql
+++ sql/a_02.sql
@@ -1,16 +1,13 @@
 --  A2
+
 DROP VIEW IF EXISTS a2;
 CREATE VIEW a2 AS
-SELECT
-        sci._id,
-        sci.mcc,
-        sci.mnc,
-        sci.lac,
-        sci.cid,
-        sci.timestamp AS timestamp,
-        sum(CASE WHEN sci.lac != nci.lac THEN 1.0 ELSE 0.0 END) as value,
-        sum(CASE WHEN sci.lac != nci.lac THEN 1.0 ELSE 0.0 END) / count(*) as score
-FROM serving_cell_info AS sci, neighboring_cell_info AS nci
-ON nci.last_sc_id = sci._id
-WHERE nci.lac > 0 AND nci.cid > 0 AND sci.mcc = nci.mcc AND sci.mnc = sci.mnc
-GROUP BY sci._id;
+SELECT DISTINCT
+        cell.mcc,
+        cell.mnc,
+        cell.lac,
+        cell.cid,
+        (sum(CASE WHEN cell.lac != neig.lac THEN 1 ELSE 0 END) + 0.0)/count(*) as count
+FROM cell_info AS cell, arfcn_list AS al, cell_info AS neig, config
+ON cell.id = al.id AND al.arfcn = neig.bcch_arfcn
+GROUP BY cell.id;
