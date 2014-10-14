0b5cf9983f08a586a0356c42a44a965455488887
Alexander Senier <alex@srlabs.de>
2014-10-14 13:17:44 +0200

    Fix time comparison and document

--- sql/a_01.sql
+++ sql/a_01.sql
@@ -1,5 +1,7 @@
 --  A1
 
+--  Select all cell_info entries which have been observed within
+--  'delta_arfcn' seconds broadcasting a different MCC/MNC/LAC/CID
 DROP VIEW IF EXISTS dup_arfcns;
 CREATE VIEW dup_arfcns AS
 SELECT
@@ -14,9 +16,14 @@ ON
          l.mnc != r.mnc OR
          l.lac != r.lac OR
          l.cid != r.cid) AND
-        (r.first_seen - l.last_seen) < config.delta_arfcn
+        (strftime('%s', l.first_seen) - strftime('%s', r.last_seen)) >= 0 AND
+        (strftime('%s', l.first_seen) - strftime('%s', r.last_seen)) < config.delta_arfcn
+WHERE
+        l.mcc > 0 AND l.mnc > 0 AND l.lac > 0 AND l.cid > 0 AND
+        r.mcc > 0 AND r.mnc > 0 AND r.lac > 0 AND r.cid > 0
 GROUP BY l.bcch_arfcn;
 
+--  Associate a1 score
 DROP VIEW IF EXISTS a1;
 CREATE VIEW a1 AS
 SELECT
@@ -30,5 +37,7 @@ SELECT
 FROM cell_info AS ci LEFT JOIN dup_arfcns AS da
 ON
         ci.bcch_arfcn = da.bcch_arfcn AND
-        ci.first_seen >= da.first_seen AND
-        ci.last_seen  <= da.last_seen;
+        strftime('%s', ci.first_seen) >= strftime('%s', da.first_seen) AND
+        strftime('%s', ci.last_seen)  <= strftime('%s', da.last_seen)
+WHERE
+        ci.mcc > 0 AND ci.mnc > 0 and ci.lac > 0 and ci.cid > 0;
