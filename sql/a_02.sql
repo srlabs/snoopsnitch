b15c75b7162e9c5b6767b1c0cd598c76193ee173
Alexander Senier <alex@srlabs.de>
2014-10-16 17:58:56 +0200

    Filter out cells with zero MCC/MNC/LAC or CID

--- sql/a_02.sql
+++ sql/a_02.sql
@@ -11,4 +11,6 @@ SELECT DISTINCT
         sum(CASE WHEN cell.lac != neig.lac THEN 1.0 ELSE 0.0 END)/count(*) as score
 FROM cell_info AS cell, arfcn_list AS al, cell_info AS neig, config
 ON cell.id = al.id AND al.arfcn = neig.bcch_arfcn
+WHERE
+        cell.mcc > 0 AND cell.mnc > 0 AND cell.lac > 0 AND cell.cid > 0
 GROUP BY cell.id;
