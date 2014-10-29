a374b5864c6b442de92b07fde890d3bf9ed6b958
Jakob Lell <jakob@srlabs.de>
2014-10-29 17:19:01 +0100

    Removed workaround for bug in libosmo-asn1-rrc build system, see task #1241

--- contrib/scripts/compile_libosmo-asn1-rrc.sh
+++ contrib/scripts/compile_libosmo-asn1-rrc.sh
@@ -3,6 +3,6 @@
 cd ${BASE_DIR}/libosmo-asn1-rrc
 
 autoreconf -fi
-./configure ${MSD_CONFIGURE_OPTS}
-make -j4 libosmo_asn1_rrc_la_LDFLAGS="-lasn1c -L${MSD_DESTDIR}/lib"
+./configure ${MSD_CONFIGURE_OPTS} LDFLAGS="-lasn1c -L${MSD_DESTDIR}/lib" 
+make -j4
 make install
