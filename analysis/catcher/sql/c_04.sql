6df1a66da4055230d312a5ac81e4b182d7b318cf
Alexander Senier <alex@srlabs.de>
2014-11-12 14:56:35 +0100

    Run analysis on domain=0 sessions only

--- analysis/catcher/sql/c_04.sql
+++ analysis/catcher/sql/c_04.sql
@@ -8,4 +8,6 @@ SELECT
         lac,
         cid,
         (CASE WHEN iden_imsi_bc AND iden_imei_bc THEN 1 WHEN iden_imsi_bc OR iden_imei_bc THEN 0.7 ELSE 0 END) as score
-FROM session_info;
+FROM session_info
+WHERE
+	domain = 0;
