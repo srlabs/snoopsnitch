9ea15aca5141c439b73c2f21fa1a887f972d0cfc
Alexander Senier <alex@srlabs.de>
2014-11-13 11:22:55 +0100

    Fix session_info/cell_info analysis merging

--- analysis/catcher/analysis_ci.sql
+++ analysis/catcher/analysis_ci.sql
@@ -20,9 +20,8 @@
 DROP VIEW IF EXISTS ci;
 CREATE VIEW ci AS
 SELECT DISTINCT
-        ci.id,
-        ci.first_seen,
-        ci.last_seen,
+        ci.first_seen as first_seen,
+        ci.last_seen as last_seen,
         ci.mcc,
         ci.mnc,
         ci.lac,
@@ -34,15 +33,7 @@ SELECT DISTINCT
         ifnull(k2.score, 0) as k2,
         ifnull(t1.score, 0) as t1,
         ifnull(r1.score, 0) as r1,
-        ifnull(r2.score, 0) as r2,
-        (ifnull(a1.score, 0) +
-         ifnull(a2.score, 0) +
-         ifnull(a4.score, 0) +
-         ifnull(k1.score, 0) +
-         ifnull(k2.score, 0) +
-         ifnull(t1.score, 0) +
-         ifnull(r1.score, 0) +
-         ifnull(r2.score, 0)) as score
+        ifnull(r2.score, 0) as r2
 FROM cell_info as ci LEFT JOIN
  a1 ON ci.id = a1.id LEFT JOIN
  a2 ON ci.id = a2.id LEFT JOIN
@@ -52,4 +43,5 @@ FROM cell_info as ci LEFT JOIN
  t1 ON ci.id = t1.id LEFT JOIN
  r1 ON ci.id = r1.id LEFT JOIN
  r2 ON ci.id = r2.id
-WHERE ci.mcc > 0 and ci.mnc > 0 and ci.lac > 0 and ci.cid > 0;
+WHERE
+	ci.mcc > 0 AND ci.mnc > 0 AND ci.lac > 0 AND ci.cid > 0;
--- analysis/catcher/analysis_si.sql
+++ analysis/catcher/analysis_si.sql
@@ -19,6 +19,7 @@ CREATE VIEW si AS
 SELECT
         si.id,
         si.timestamp,
+        si.duration/1000 as duration,
         si.mcc,
         si.mnc,
         si.lac,
@@ -29,14 +30,7 @@ SELECT
         ifnull(c4.score, 0) as c4,
         ifnull(t3.score, 0) as t3,
         ifnull(t4.score, 0) as t4,
-        ifnull(f1.score, 0) as f1,
-        (ifnull(c1.score, 0) +
-         ifnull(c2.score, 0) +
-         ifnull(c3.score, 0) +
-         ifnull(c4.score, 0) +
-         ifnull(t3.score, 0) +
-         ifnull(t4.score, 0) +
-         ifnull(f1.score, 0)) as score
+        ifnull(f1.score, 0) as f1
 FROM session_info as si LEFT JOIN
     c1 ON si.id = c1.id LEFT JOIN
     c2 ON si.id = c2.id LEFT JOIN
