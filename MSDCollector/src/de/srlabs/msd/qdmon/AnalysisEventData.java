2ac3920913a71c6e22fb20f349413bd234c64e1c
Alexander Senier <alex@srlabs.de>
2014-12-01 10:28:25 +0100

    Read GSMmap data from file
    TODO: Data is read from SD card right now. Add a static version to assets
    and revoke external storage permissions. Later, retrieve data.js from
    webserver and parse this version.

--- MSDCollector/src/de/srlabs/msd/qdmon/AnalysisEventData.java
+++ MSDCollector/src/de/srlabs/msd/qdmon/AnalysisEventData.java
@@ -1,27 +1,136 @@
 package de.srlabs.msd.qdmon;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Vector;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 import android.util.Log;
 
 import de.srlabs.msd.analysis.ImsiCatcher;
+import de.srlabs.msd.analysis.Risk;
 import de.srlabs.msd.analysis.SMS;
 import de.srlabs.msd.analysis.SMS.Type;
 import de.srlabs.msd.util.MsdDatabaseManager;
 
 public class AnalysisEventData implements AnalysisEventDataInterface{
-	private Context context;
 	private SQLiteDatabase db;
 
 	public AnalysisEventData(Context context) {
 
+		long operator_id = 0;
+		JSONObject gsmmapData;
 		MsdDatabaseManager.initializeInstance(new MsdSQLiteOpenHelper(context));
 
-		this.context = context;
 		this.db = MsdDatabaseManager.getInstance().openDatabase();
+
+		File sdcard = Environment.getExternalStorageDirectory();
+		File file = new File(sdcard, "data.js");
+		StringBuilder text = new StringBuilder();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = br.readLine()) != null) {
+				text.append(line);
+			}
+			br.close();
+
+			gsmmapData = new JSONObject(text.toString().split("^\\s*var\\s*data\\s*=\\s*", 2)[1]);
+
+			db.delete("gsmmap_operators", null, null);
+			db.delete("gsmmap_codes", null, null);
+			db.delete("gsmmap_inter", null, null);
+			db.delete("gsmmap_imper", null, null);
+			db.delete("gsmmap_track", null, null);
+			db.delete("gsmmap_inter3G", null, null);
+			db.delete("gsmmap_imper3G", null, null);
+
+			// Iterate overall countries
+			JSONArray countries = gsmmapData.getJSONArray("countries");
+			for (int c = 0, cs = countries.length(); c < cs; c++) {
+
+				JSONObject country = countries.getJSONObject(c);
+
+				// Iterate over all operators
+				JSONArray operators = country.getJSONArray("operators");
+				for (int o = 0, os = operators.length(); o < os; o++) {
+
+					JSONObject operator = operators.getJSONObject(o);
+
+					String operator_name = operator.get("name").toString();
+					String operator_color = operator.has("color") ? operator.get("color").toString() : null;
+
+					// Store operator in database
+					ContentValues opval = new ContentValues();
+					opval.put("id", operator_id);
+					opval.put("name", operator_name);
+					opval.put("color", operator_color);
+					db.insert("gsmmap_operators", null, opval);
+					opval.clear();
+
+					// Store all values
+					JSONObject values = operator.getJSONObject("values");
+					updateValue(operator_id, values, "inter");
+					updateValue(operator_id, values, "imper");
+					updateValue(operator_id, values, "track");
+					updateValue(operator_id, values, "inter3G");
+					updateValue(operator_id, values, "imper3G");
+
+					// Store all MCC/MNC combinations for an operator
+					JSONArray mcc_mncs = operator.getJSONArray("mcc_mnc");
+					for (int m = 0, ms = mcc_mncs.length(); m < ms; m++) {
+
+						JSONObject mcc_mnc = mcc_mncs.getJSONObject(m);
+						int mcc = mcc_mnc.getInt("mcc");
+						int mnc = mcc_mnc.getInt("mnc");
+
+						ContentValues codes_val = new ContentValues();
+						codes_val.put("id", operator_id);
+						codes_val.put("mcc", mcc);
+						codes_val.put("mnc", mnc);
+						db.insert("gsmmap_codes", null, codes_val);
+						codes_val.clear();
+					}
+					operator_id++;
+				}
+			}
+
+		} catch (IOException e) {
+		} catch (JSONException e) {
+		}
+	}
+
+	private void updateValue(long operator_id, JSONObject values, String kind)
+			throws JSONException {
+		if (values.has(kind)) {
+			JSONArray value = values.getJSONArray(kind);
+			for (int v = 0, vs = value.length(); v < vs; v++) {
+
+				JSONObject val = value.getJSONObject(v);
+				int monthIndex = val.getInt("monthIndex");
+				int year  = 2011 + monthIndex / 12;
+				int month = monthIndex % 12 + 1;
+
+				ContentValues cont = new ContentValues();
+				cont.put("id", operator_id);
+				cont.put("year", Integer.toString(year));
+				cont.put("month", Integer.toString(month));
+				cont.put("value", val.getDouble("value"));
+				db.insert("gsmmap_" + kind, null, cont);
+				cont.clear();
+			}
+		}
 	}
 
 	private static String[] sms_cols =
