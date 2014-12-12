110d4402633ec5e960fbfa6f0cc25627191bad6c
Alexander Senier <alex@srlabs.de>
2014-12-12 13:23:42 +0100

    Treat location updates separately in C1 criterion
    This resolves a false positive where a different algorithm is used for
    locations updates. This has been observed in the Vodafone network in cell
    262/2/308/37501, where they use A5/1 for location updates and A5/3 for other
    transactions.

--- analysis/catcher/sql/c_01.sql
+++ analysis/catcher/sql/c_01.sql
@@ -1,9 +1,9 @@
 DROP VIEW IF EXISTS max_cipher;
 CREATE VIEW max_cipher AS
-SELECT mcc, mnc, lac, cid, max(cipher) as max
+SELECT mcc, mnc, lac, cid, t_locupd, max(cipher) as max
 FROM session_info
 WHERE domain = 0 AND mcc > 0 AND mnc > 0 AND lac > 0 AND cid > 0
-GROUP BY mcc, mnc, lac, cid;
+GROUP BY mcc, mnc, lac, cid, t_locupd;
 
 DROP VIEW IF EXISTS c1;
 CREATE VIEW c1 AS
@@ -18,5 +18,5 @@ SELECT
         mc.max as threshold,
         cipher < mc.max AS score
 FROM session_info AS si, max_cipher as mc
-ON si.mcc = mc.mcc AND si.mnc = mc.mnc AND si.lac = mc.lac AND si.cid = mc.cid
+ON si.mcc = mc.mcc AND si.mnc = mc.mnc AND si.lac = mc.lac AND si.cid = mc.cid AND si.t_locupd = mc.t_locupd
 WHERE domain = 0;
