c794c5e6eec495b753901a2fe359dbb184decf7a
Joachim Wiemes <joachim.wiemes@uid.com>
2014-12-17 10:58:21 +0100

    Open URL of local map view with mcc parameter.

--- MSDCollector/src/de/srlabs/msd/LocalMapActivity.java
+++ MSDCollector/src/de/srlabs/msd/LocalMapActivity.java
@@ -21,7 +21,9 @@ public class LocalMapActivity extends BaseActivity
 	        
 	        // We have to set margin/padding to zero to avoid
 	        // a white border around the web view
-	        String customHtml = "<html><head><style>* {margin:0;padding:0;}</style></head><body><iframe src=\"http://gsmmap.org/?n=0\" width=\"100%\" height=\"100%\" scrolling=\"auto\" frameborder=\"0\" ></iframe></body></html>";
+	        String mcc = String.valueOf(msdServiceHelperCreator.getMsdServiceHelper().getData().getScores().getMcc());
+	        String customHtml = "<html><head><style>* {margin:0;padding:0;}</style></head><body><iframe src=\"http://gsmmap.org/?n=" 
+	        		+ mcc + "\" width=\"100%\" height=\"100%\" scrolling=\"auto\" frameborder=\"0\" ></iframe></body></html>";
 	        webview.loadData(customHtml, "text/html", "UTF-8");
 		}
 		catch (Exception e)
