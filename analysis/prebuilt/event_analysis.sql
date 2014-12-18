9db0b4e656a7de9d0f92c5bde8e957e75a79d1d3
Alexander Senier <alex@srlabs.de>
2014-12-18 18:33:28 +0100

    Separate analysis of SMS and session_based events
    This fixes an issue where null-pagings were not detected

--- analysis/prebuilt/event_analysis.sql
+++ analysis/prebuilt/event_analysis.sql
@@ -10,10 +10,8 @@ SELECT
 	sl.longitude,
 	sl.latitude,
 	sl.valid,
-	--  FIXME: Handle SMSC/MSISDN null values in UI properly
-	--  by destinguishing different event types
-	ifnull(sm.smsc, "-"),
-	ifnull(sm.msisdn, "-"),
+	sm.smsc,
+	sm.msisdn,
 	CASE
 		-- OTA/binary SMS
 		WHEN ota AND from_network
@@ -21,15 +19,12 @@ SELECT
 		-- silent SMS
 		WHEN dcs >= 192 AND dcs <= 207 AND from_network
 			THEN 2
-		-- Null paging
-		WHEN mobile_term AND NOT t_call AND NOT t_sms AND NOT t_locupd AND t_abort
-			THEN 3
 		-- All other transaction we don't care about
 		ELSE 0
 	END as event_type
 FROM
 	session_info as si,
-	si_loc as sl LEFT JOIN
+	si_loc as sl,
 	sms_meta as sm
 ON
 	sm.id = si.id AND
@@ -37,3 +32,23 @@ ON
 WHERE
 	event_type > 0 AND
 	domain = 0;
+
+INSERT INTO events
+SELECT
+	si.id,
+	si.timestamp,
+	si.mcc,
+	si.mnc,
+	si.lac,
+	si.cid,
+	0.0,
+	0.0,
+	0,
+	"-",
+	"-",
+	3 as event_type
+FROM
+	session_info as si
+WHERE
+	domain = 0 AND
+	mobile_term AND NOT t_call AND NOT t_sms AND NOT t_locupd AND t_abort;
