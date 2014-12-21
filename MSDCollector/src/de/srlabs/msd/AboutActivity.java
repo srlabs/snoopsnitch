5a618e48ede59695f96e77742a6fc6700d466b37
Alexander Senier <alex@srlabs.de>
2014-12-21 01:28:15 +0100

    Remove map/recording button from about dialog

--- MSDCollector/src/de/srlabs/msd/AboutActivity.java
+++ MSDCollector/src/de/srlabs/msd/AboutActivity.java
@@ -1,6 +1,7 @@
 package de.srlabs.msd;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class AboutActivity extends BaseActivity
 {
@@ -10,5 +11,17 @@ public class AboutActivity extends BaseActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu _menu)
+	{
+		super.onCreateOptionsMenu(_menu);
+
+		// Disable map, recording icon and preferences
+		menu.getItem(0).setVisible(false);
+		menu.getItem(1).setVisible(false);
+
+		return true;
 	}
 }
