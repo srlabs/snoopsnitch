de23abad1d33a7640c7f12e59decdc6e064507cf
Alexander Senier <alex@srlabs.de>
2014-12-28 15:28:51 +0100

    Disable F1 for now, as it causes performance issues
    We need to fix the query once we make use of F1

--- analysis/catcher/sql/f_01.sql
+++ analysis/catcher/sql/f_01.sql
@@ -1,24 +1,35 @@
 DROP VIEW IF EXISTS f1;
+-- CREATE VIEW f1 AS
+-- SELECT
+-- 	si.id,
+-- 	min(si2.id),
+-- 	si.mcc,
+-- 	si.mnc,
+-- 	si.lac,
+-- 	si.cid,
+-- 	count(pag1_rate) as count,
+-- 	avg(pag1_rate) < config.min_pag1_rate as score
+-- FROM
+-- 	session_info AS si, session_info as si2, paging_info AS pi, config
+-- ON
+-- 	--  Join on the next location update…
+-- 	si2.id > si.id AND
+-- 	--  …all paging messages since the current location update started…
+-- 	strftime('%s', pi.timestamp) - strftime('%s', si.timestamp - si.duration/1000) > 0 AND
+-- 	--  …until the succeeding location update starts…
+-- 	strftime('%s', si2.timestamp) - si2.duration/1000 - strftime('%s', pi.timestamp) > 0
+-- WHERE
+-- 	si.domain = 0 AND si2.domain = 0 AND si.lu_acc AND si2.lu_acc
+-- GROUP BY
+-- 	si.id
+
 CREATE VIEW f1 AS
 SELECT
-	si.id,
-	min(si2.id),
-	si.mcc,
-	si.mnc,
-	si.lac,
-	si.cid,
-	count(pag1_rate) as count,
-	avg(pag1_rate) < config.min_pag1_rate as score
-FROM
-	session_info AS si, session_info as si2, paging_info AS pi, config
-ON
-	--  Join on the next location update…
-	si2.id > si.id AND
-	--  …all paging messages since the current location update started…
-	strftime('%s', pi.timestamp) - strftime('%s', si.timestamp - si.duration/1000) > 0 AND
-	--  …until the succeeding location update starts…
-	strftime('%s', si2.timestamp) - si2.duration/1000 - strftime('%s', pi.timestamp) > 0
-WHERE
-	si.domain = 0 AND si2.domain = 0 AND si.lu_acc AND si2.lu_acc
-GROUP BY
-	si.id;
+	0 as id,
+	0,
+	0 as mcc,
+	0 as mnc,
+	0 as lac,
+	0 as cid,
+	0 as count,
+	0 as score;
--- analysis/prebuilt/catcher_analysis.sql
+++ analysis/prebuilt/catcher_analysis.sql
@@ -418,29 +418,40 @@ FROM cell_info, config;
 
 --  Fingerprint
 DROP VIEW IF EXISTS f1;
+-- CREATE VIEW f1 AS
+-- SELECT
+-- 	si.id,
+-- 	min(si2.id),
+-- 	si.mcc,
+-- 	si.mnc,
+-- 	si.lac,
+-- 	si.cid,
+-- 	count(pag1_rate) as count,
+-- 	avg(pag1_rate) < config.min_pag1_rate as score
+-- FROM
+-- 	session_info AS si, session_info as si2, paging_info AS pi, config
+-- ON
+-- 	--  Join on the next location update…
+-- 	si2.id > si.id AND
+-- 	--  …all paging messages since the current location update started…
+-- 	strftime('%s', pi.timestamp) - strftime('%s', si.timestamp - si.duration/1000) > 0 AND
+-- 	--  …until the succeeding location update starts…
+-- 	strftime('%s', si2.timestamp) - si2.duration/1000 - strftime('%s', pi.timestamp) > 0
+-- WHERE
+-- 	si.domain = 0 AND si2.domain = 0 AND si.lu_acc AND si2.lu_acc
+-- GROUP BY
+-- 	si.id
+
 CREATE VIEW f1 AS
 SELECT
-	si.id,
-	min(si2.id),
-	si.mcc,
-	si.mnc,
-	si.lac,
-	si.cid,
-	count(pag1_rate) as count,
-	avg(pag1_rate) < config.min_pag1_rate as score
-FROM
-	session_info AS si, session_info as si2, paging_info AS pi, config
-ON
-	--  Join on the next location update…
-	si2.id > si.id AND
-	--  …all paging messages since the current location update started…
-	strftime('%s', pi.timestamp) - strftime('%s', si.timestamp - si.duration/1000) > 0 AND
-	--  …until the succeeding location update starts…
-	strftime('%s', si2.timestamp) - si2.duration/1000 - strftime('%s', pi.timestamp) > 0
-WHERE
-	si.domain = 0 AND si2.domain = 0 AND si.lu_acc AND si2.lu_acc
-GROUP BY
-	si.id;
+	0 as id,
+	0,
+	0 as mcc,
+	0 as mnc,
+	0 as lac,
+	0 as cid,
+	0 as count,
+	0 as score;
 
 --  Result
 DROP VIEW IF EXISTS ci;
@@ -509,9 +520,9 @@ SELECT
 	max(ci.r1),
 	max(ci.r2),
 	max(ci.f1),
-	ifnull(si_loc.longitude, 0.0),
-	ifnull(si_loc.latitude, 0.0),
-	ifnull(si_loc.valid, 0),
+	si_loc.longitude,
+	si_loc.latitude,
+	si_loc.valid,
 	max(ci.a1) +
 	max(ci.a2) +
 	max(ci.a4) +
@@ -528,7 +539,7 @@ SELECT
 	max(ci.r1) +
 	max(ci.r2) +
 	max(ci.f1) as score
-FROM si LEFT JOIN ci LEFT JOIN si_loc
+FROM si LEFT JOIN ci, si_loc
 ON
 	ci.mcc = si.mcc AND
 	ci.mnc = si.mnc AND
