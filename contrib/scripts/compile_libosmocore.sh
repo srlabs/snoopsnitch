3c33eec08faec0e51a1f6d7a7da3572f1bb7f0ce
Alexander Senier <alex@srlabs.de>
2015-01-19 23:38:19 +0100

    Allow for building on OS X hosts

--- contrib/scripts/compile_libosmocore.sh
+++ contrib/scripts/compile_libosmocore.sh
@@ -5,6 +5,7 @@ git reset --hard
 git clean -f
 patch -p1 < ${BASE_DIR}/patches/osmocore_luca.patch
 patch -p1 < ${BASE_DIR}/patches/osmocore_jakob.patch
+patch -p1 < ${BASE_DIR}/patches/osmocore_osx.patch
 
 autoreconf -fi
 ./configure ${MSD_CONFIGURE_OPTS} --disable-utilities --disable-serial --enable-talloc
