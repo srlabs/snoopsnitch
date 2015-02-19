29091ab01d8827229afc9dccc565ac84542217ad
Alexander Senier <alex@srlabs.de>
2015-02-19 18:55:38 +0100

    Only consider accepted location updates in max_cipher

--- analysis/catcher/sql/c_01.sql
+++ analysis/catcher/sql/c_01.sql
@@ -1,9 +1,9 @@
 DROP VIEW IF EXISTS max_cipher;
 CREATE VIEW max_cipher AS
-SELECT mcc, mnc, lac, cid, t_locupd, max(cipher) as max
+SELECT mcc, mnc, lac, cid, lu_acc, max(cipher) as max
 FROM session_info
-WHERE domain = 0 AND mcc > 0 AND mnc > 0 AND lac > 0 AND cid > 0 AND (t_release OR t_abort)
-GROUP BY mcc, mnc, lac, cid, t_locupd;
+WHERE domain = 0 AND mcc > 0 AND mnc > 0 AND lac > 0 AND cid > 0 AND (lu_acc OR call_presence OR sms_presence)
+GROUP BY mcc, mnc, lac, cid, lu_acc;
 
 DROP VIEW IF EXISTS c1;
 CREATE VIEW c1 AS
@@ -18,5 +18,5 @@ SELECT
         mc.max as threshold,
         cipher < mc.max AS score
 FROM session_info AS si, max_cipher as mc
-ON si.mcc = mc.mcc AND si.mnc = mc.mnc AND si.lac = mc.lac AND si.cid = mc.cid AND si.t_locupd = mc.t_locupd
+ON si.mcc = mc.mcc AND si.mnc = mc.mnc AND si.lac = mc.lac AND si.cid = mc.cid AND si.lu_acc = mc.lu_acc
 WHERE domain = 0;
