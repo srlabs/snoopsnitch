d39f31cd3a50fc4422d2f287cacc53125971033c
Alexander Senier <alex@srlabs.de>
2015-02-20 17:11:20 +0100

    Make catcher analysis robust against missing location/cell info

--- analysis/prebuilt/catcher_analysis.sql
+++ analysis/prebuilt/catcher_analysis.sql
@@ -513,58 +513,60 @@ DELETE FROM catcher;
 INSERT INTO catcher
 SELECT
 	si.id,
-	ci.mcc,
-	ci.mnc,
-	ci.lac,
-	ci.cid,
+	si.mcc,
+	si.mnc,
+	si.lac,
+	si.cid,
 	si.timestamp,
 	si.duration,
-	max(ci.a1),
-	max(ci.a2),
-	max(ci.a4),
-	max(ci.k1),
-	max(ci.k2),
-	max(si.c1),
-	max(si.c2),
-	max(si.c3),
-	max(si.c4),
-	max(si.c5),
-	max(ci.t1),
-	max(si.t3),
-	max(si.t4),
-	max(ci.r1),
-	max(ci.r2),
-	max(ci.f1),
-	si_loc.longitude,
-	si_loc.latitude,
-	si_loc.valid,
-	max(ci.a1) +
-	max(ci.a2) +
-	max(ci.a4) +
-	max(ci.k1) +
-	max(ci.k2) +
-	max(si.c1) +
-	max(si.c2) +
-	max(si.c3) +
-	max(si.c4) +
-	max(si.c5) +
-	max(ci.t1) +
-	max(si.t3) +
-	max(si.t4) +
-	max(ci.r1) +
-	max(ci.r2) +
-	max(ci.f1) as score
-FROM si LEFT JOIN ci, si_loc
+	ifnull(max(ci.a1), 0.0),
+	ifnull(max(ci.a2), 0.0),
+	ifnull(max(ci.a4), 0.0),
+	ifnull(max(ci.k1), 0.0),
+	ifnull(max(ci.k2), 0.0),
+	ifnull(max(si.c1), 0.0),
+	ifnull(max(si.c2), 0.0),
+	ifnull(max(si.c3), 0.0),
+	ifnull(max(si.c4), 0.0),
+	ifnull(max(si.c5), 0.0),
+	ifnull(max(ci.t1), 0.0),
+	ifnull(max(si.t3), 0.0),
+	ifnull(max(si.t4), 0.0),
+	ifnull(max(ci.r1), 0.0),
+	ifnull(max(ci.r2), 0.0),
+	ifnull(max(ci.f1), 0.0),
+	ifnull(si_loc.longitude, 0.0),
+	ifnull(si_loc.latitude, 0.0),
+	ifnull(si_loc.valid, 0),
+	ifnull(max(ci.a1), 0.0) +
+	ifnull(max(ci.a2), 0.0) +
+	ifnull(max(ci.a4), 0.0) +
+	ifnull(max(ci.k1), 0.0) +
+	ifnull(max(ci.k2), 0.0) +
+	ifnull(max(si.c1), 0.0) +
+	ifnull(max(si.c2), 0.0) +
+	ifnull(max(si.c3), 0.0) +
+	ifnull(max(si.c4), 0.0) +
+	ifnull(max(si.c5), 0.0) +
+	ifnull(max(ci.t1), 0.0) +
+	ifnull(max(si.t3), 0.0) +
+	ifnull(max(si.t4), 0.0) +
+	ifnull(max(ci.r1), 0.0) +
+	ifnull(max(ci.r2), 0.0) +
+	ifnull(max(ci.f1), 0.0) as score
+FROM si LEFT JOIN ci
 ON
 	ci.mcc = si.mcc AND
 	ci.mnc = si.mnc AND
 	ci.lac = si.lac AND
 	ci.cid = si.cid AND
+	abs(strftime('%s', ci.last_seen) - strftime('%s', si.timestamp)) < 10000
+LEFT JOIN si_loc
+ON
 	si.id = si_loc.id,
 config
-ON
-	abs(strftime('%s', ci.last_seen) - strftime('%s', si.timestamp)) < 10000
 GROUP BY
 	si.id
 HAVING
-	score > config.catcher_min_score;
+	score > config.catcher_min_score
+;
