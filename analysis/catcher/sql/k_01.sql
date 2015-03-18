c4426626cc46c15312f2355badc68702734c3423
Alexander Senier <alex@srlabs.de>
2015-03-18 12:50:21 +0100

    Fix K1 to use the neighbor count from SI5*

--- analysis/catcher/sql/k_01.sql
+++ analysis/catcher/sql/k_01.sql
@@ -1,25 +1,15 @@
---  FIXME: Disable this test until we have the ARFCN/cell collection issue
---  sorted out, as it leads to false positives.
-
 --  K1
 DROP VIEW IF EXISTS k1;
 CREATE VIEW k1 AS
 SELECT
-	0 as id,
-	0 as mcc,
-	0 as mnc,
-	0 as lac,
-	0 as cid,
-	0.0 as score;
-
---  CREATE VIEW k1 AS
---  SELECT
---          cell.id,
---          cell.mcc,
---          cell.mnc,
---          cell.lac,
---          cell.cid,
---          sum(CASE WHEN ifnull(al.arfcn, 0) > 0 THEN 0 ELSE 1 END) = 0 as score
---  FROM cell_info AS cell LEFT JOIN arfcn_list AS al
---  ON cell.id = al.id
---  GROUP BY cell.id
+	id,
+	mcc,
+	mnc,
+	lac,
+	cid,
+	CASE
+		WHEN (neigh_5 + neigh_5b + neigh_5t) > 1 THEN 0.0
+		ELSE 1.0
+	END as score
+	FROM cell_info
+	WHERE si5 OR si5b OR si5t;
