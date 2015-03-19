53543b746edab995512d495f4df7ba21e649c267
Jakob Lell <jakob@srlabs.de>
2015-03-19 16:06:45 +0100

    Fix android.database.sqlite.SQLiteConstraintException in openOrReopenDebugLog()

--- SnoopSnitch/src/de/srlabs/snoopsnitch/qdmon/MsdService.java
+++ SnoopSnitch/src/de/srlabs/snoopsnitch/qdmon/MsdService.java
@@ -1938,7 +1938,12 @@ public class MsdService extends Service{
 		for(int i=0;i<10;i++){
 			plaintextFilename = "debug_" + timestampStr + (i>0 ? "." + i : "") +  ".gz";
 			encryptedFilename = plaintextFilename + ".smime";
-			if((new File(getFilesDir().toString() + "/" + plaintextFilename)).exists() || (new File(getFilesDir().toString() + "/" + encryptedFilename)).exists() ){
+			Cursor cur = db.query("files", null, "filename='" + encryptedFilename + "'", null, null, null, "_id");
+			boolean existingInDb = cur.moveToFirst();
+			cur.close();
+			if(existingInDb ||
+					(new File(getFilesDir().toString() + "/" + plaintextFilename)).exists() ||
+					(new File(getFilesDir().toString() + "/" + encryptedFilename)).exists() ){
 				plaintextFilename = null;
 				encryptedFilename = null;
 			} else{
