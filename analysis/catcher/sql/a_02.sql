6a230bcd231e5494b66956343bf8531cce831c79
Alexander Senier <alex@srlabs.de>
2015-03-19 09:47:04 +0100

    Improve A2 calculation

--- analysis/catcher/sql/a_02.sql
+++ analysis/catcher/sql/a_02.sql
@@ -1,16 +1,29 @@
 --  A2
-
 DROP VIEW IF EXISTS a2;
 CREATE VIEW a2 AS
 SELECT DISTINCT
-        cell.id,
-        cell.mcc,
-        cell.mnc,
-        cell.lac,
-        cell.cid,
-        sum(CASE WHEN cell.lac != neig.lac THEN 1.0 ELSE 0.0 END)/count(*) as score
-FROM cell_info AS cell, arfcn_list AS al, cell_info AS neig, config
-ON cell.id = al.id AND al.arfcn = neig.bcch_arfcn
+	cell.id,
+	cell.mcc,
+	cell.mnc,
+	cell.lac,
+	cell.cid,
+	--  If none of the observed neighbors has the same LAC as this cell the score is 1.0
+	0.0 + ((count(*) - sum(case when cell.lac != neig.lac then 1 else 0 end)) = 0) as score
+FROM
+	cell_info  AS cell,
+	arfcn_list AS al,
+	cell_info  AS neig,
+	config
+ON
+	cell.id = al.id AND
+	al.arfcn = neig.bcch_arfcn AND
+	cell.bcch_arfcn != neig.bcch_arfcn AND
+	cell.mcc = neig.mcc AND
+	cell.mnc = neig.mnc
 WHERE
-        cell.mcc > 0 AND cell.mnc > 0 AND cell.lac > 0 AND cell.cid > 0
+	cell.mcc > 0 AND
+	cell.mnc > 0 AND
+	cell.lac > 0 AND
+	cell.cid > 0 AND
+	neig.lac > 0
 GROUP BY cell.id;
