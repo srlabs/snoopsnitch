2f40cf57504ae0273fe0496efc9935d5e8ad2463
Alexander Senier <alex@srlabs.de>
2015-03-19 18:25:36 +0100

    Fix R1 score

--- analysis/catcher/sql/r_01.sql
+++ analysis/catcher/sql/r_01.sql
@@ -2,7 +2,7 @@
 
 --  For every cell info for which a MCC/MNC/LAC/CID was recorded,
 --  add a column with the neighboring ARFCN for each entry created
---  from BCCH/SI2 with the same ID in the arfcn_list table.
+--  from with the same ID in the arfcn_list table.
 DROP VIEW IF EXISTS cells_with_neig_arfcn;
 CREATE VIEW cells_with_neig_arfcn AS
 SELECT
@@ -15,7 +15,7 @@ SELECT
         al.arfcn as neig_arfcn
 FROM cell_info AS ci LEFT JOIN arfcn_list AS al
 ON ci.id = al.id
-WHERE ci.mcc > 0 AND ci.mnc > 0 AND ci.lac > 0 AND ci.cid > 0 AND al.source = "SI2";
+WHERE ci.mcc > 0 AND ci.mnc > 0 AND ci.lac > 0 AND ci.cid > 0;
 
 --  Join the cells_with_neig_arfcn table above with itself, such
 --  that that for every cell in cell_info we get the respective 
@@ -37,15 +37,15 @@ ON c.neig_arfcn = n.bcch_arfcn;
 
 --  Count the number of neighboring cells recorded for every cell
 --  in the table. Note, that this is the number of neighboring
---  cells we actually received SI2 information for (as an inner
+--  cells we actually received neighbor information for (as an inner
 --  join is used above). This count will later be used as the
 --  number of neighboring cells that should have the cell in their
 --  own ARFCN list.
---  This makes sense, as you may not receive BCCH info from ap
+--  This makes sense, as you may not receive BCCH info from a
 --  neighboring cell on the far other end of you current cell.
 --  This is normal and should not lead to a high score. This is
 --  only the case if a (supposed) neighboring cell that we
---  receive SI2 info from does not announce the current cell as
+--  receive SI messages from does not announce the current cell as
 --  their own neighbor.
 DROP VIEW IF EXISTS cells_neig_count;
 CREATE VIEW cells_neig_count AS
@@ -55,8 +55,8 @@ GROUP BY id, cell_mcc, cell_mnc, cell_lac, cell_cid;
 
 --  This is the list neighboring cells that reference a cell.
 --  It is similar to cells_with_neig_cell, just that we also
---  join on the neigboring cells ARFCN from SI2 equalling 
---  the current cells main ARFCN.
+--  join on the neigboring cells ARFCN equaling the current
+--  cells main ARFCN.
 DROP VIEW IF EXISTS cells_ref_by_neig_cell;
 CREATE VIEW cells_ref_by_neig_cell AS
 SELECT DISTINCT
@@ -70,7 +70,7 @@ SELECT DISTINCT
 FROM cells_with_neig_arfcn as c, cells_with_neig_arfcn as n
 ON c.neig_arfcn = n.bcch_arfcn AND c.bcch_arfcn = n.neig_arfcn;
 
---  Count by how many neigboring cells a cell is announce
+--  Count by how many neigboring cells a cell is announced
 --  as neighboring cell.
 DROP VIEW IF EXISTS cells_ref_by_neig_count;
 CREATE VIEW cells_ref_by_neig_count AS
@@ -79,10 +79,8 @@ FROM cells_ref_by_neig_cell
 GROUP BY cell_mcc, cell_mnc, cell_lac, cell_cid, cell_arfcn
 ORDER BY id;
 
---  Join both counts. For each cell, divide the number of
---  reachable neigboring cells by which the cell is announced
---  by the number of neighboring cells that should announce
---  it.
+--  Join both counts. If none of the neighbors announce
+--  this cell, assign a 1.0 score
 DROP VIEW IF EXISTS r1;
 CREATE VIEW r1 AS
 SELECT 
@@ -91,6 +89,9 @@ SELECT
         cn.cell_mnc AS mnc,
         cn.cell_lac AS lac,
         cn.cell_cid AS cid,
-        1 - (crbn.count + 0.0)/cn.count as score
+		CASE
+			WHEN cn.count - crbn.count = 0 THEN 1.0
+										   ELSE 0.0
+		END as score
 FROM cells_neig_count as cn, cells_ref_by_neig_count as crbn
 ON cn.id = crbn.id;
