ee4411e373c75b53229f70a9f0e3b40ca94676bc
Luca Melette <luca@srlabs.de>
2015-07-08 15:36:09 +0200

    Improved cipher downgrade detection with timestamp

--- analysis/catcher/sql/c_01.sql
+++ analysis/catcher/sql/c_01.sql
@@ -5,6 +5,14 @@ FROM session_info
 WHERE domain = 0 AND mcc > 0 AND lac > 0 AND cid > 0 AND (lu_acc OR call_presence OR sms_presence)
 GROUP BY mcc, mnc, lac, cid, lu_acc;
 
+DROP VIEW IF EXISTS best_cipher;
+CREATE VIEW best_cipher AS
+SELECT mc.mcc, mc.mnc, mc.lac, mc.cid, mc.lu_acc, mc.max, min(si.timestamp) as upgraded_since
+FROM max_cipher as mc, session_info as si
+ON si.mcc = mc.mcc AND si.mnc = mc.mnc AND si.lac = mc.lac AND si.cid = mc.cid AND si.lu_acc = mc.lu_acc and si.cipher = mc.max
+WHERE domain = 0
+GROUP BY mc.mcc, mc.mnc, mc.lac, mc.cid, mc.lu_acc;
+
 DROP VIEW IF EXISTS c1;
 CREATE VIEW c1 AS
 SELECT
@@ -15,8 +23,8 @@ SELECT
         si.lac,
         si.cid,
         cipher as value,
-        mc.max as threshold,
-        cipher < mc.max AS score
-FROM session_info AS si, max_cipher as mc
-ON si.mcc = mc.mcc AND si.mnc = mc.mnc AND si.lac = mc.lac AND si.cid = mc.cid AND si.lu_acc = mc.lu_acc
+        bc.max as threshold,
+        (si.timestamp > bc.upgraded_since) AND (si.cipher < bc.max) AS score
+FROM session_info AS si, best_cipher as bc
+ON si.mcc = bc.mcc AND si.mnc = bc.mnc AND si.lac = bc.lac AND si.cid = bc.cid AND si.lu_acc = bc.lu_acc
 WHERE domain = 0 AND (si.lu_acc OR si.call_presence OR si.sms_presence);
