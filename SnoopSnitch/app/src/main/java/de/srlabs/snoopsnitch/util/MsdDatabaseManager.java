0667548851a969ce9148c19226d5d11b47b0c75f
Jonas Schmid <jonas@srlabs.de>
2017-09-06 16:38:04 +0200

    Bugfix: When writeable DB object can not be retrieved after 3 tries (with 1s offset), throw an IllegalStateException and let the App crash

--- SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/util/MsdDatabaseManager.java
+++ SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/util/MsdDatabaseManager.java
@@ -18,6 +18,10 @@ public class MsdDatabaseManager {
     private static MsdSQLiteOpenHelper mDatabaseHelper;
     private SQLiteDatabase mDatabase;
 
+    private int numFailedDBRetrievingTries = 0;
+    private static final int MAX_FAILED_DB_RETRV_TRIES = 3;
+    private static final int SECONDS_DELAY_BETWEEN_DB_RETRV_TRY = 1;
+
     public static synchronized void initializeInstance(MsdSQLiteOpenHelper helper) {
         if (instance == null) {
             instance = new MsdDatabaseManager();
@@ -34,7 +38,7 @@ public class MsdDatabaseManager {
         return instance;
     }
 
-    public synchronized SQLiteDatabase openDatabase() {
+    public synchronized SQLiteDatabase openDatabase() throws IllegalStateException {
         if (mOpenCounter.incrementAndGet() == 1) {
             // Opening new database
             try {
@@ -42,6 +46,23 @@ public class MsdDatabaseManager {
             }
             catch (SQLException e){
                 Log.e(TAG,"SQLException when trying to retrieve writeable DB object: "+e.getMessage());
+
+                numFailedDBRetrievingTries++;
+                if(numFailedDBRetrievingTries <= MAX_FAILED_DB_RETRV_TRIES){
+                    // try again after a predefined delay
+                    try {
+                        Thread.sleep(1000L * SECONDS_DELAY_BETWEEN_DB_RETRV_TRY);
+                    } catch (InterruptedException e1) {
+                        Log.w(TAG,"Sleeping before retrying to access DB again was interrupted: " + e1.getMessage());
+                    }
+
+                    Log.w(TAG,"Trying to retrieve writeabele DB object again.");
+                    return openDatabase();
+                }
+                else{
+                   // cause App crash
+                   throw new IllegalStateException("Retrieving writeable DB object not possible.");
+                }
             }
         }
         return mDatabase;
