a51e5bdc1afccd97e9b4355d659702f35bdd59f1
Jonas Schmid <jonas@srlabs.de>
2018-04-10 13:08:47 +0200

    Simple logging in boot completed receiver, to check that it was running

--- SnoopSnitch/app/src/main/java/de/srlabs/patchalyzer/BootCompletedBroadcastReceiver.java
+++ SnoopSnitch/app/src/main/java/de/srlabs/patchalyzer/BootCompletedBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import de.srlabs.patchalyzer.analysis.TestUtils;
 import de.srlabs.patchalyzer.helpers.NotificationHelper;
@@ -18,6 +19,7 @@ public class BootCompletedBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(Constants.LOG_TAG,"Boot completed event received...");
         SharedPreferences sharedPrefs = SharedPrefsHelper.getPersistentSharedPrefs(context);
         long currentBuildDate = TestUtils.getBuildDateUtc();
         long buildDateUtcAtLastSuccessfulAnalysis = sharedPrefs.getLong(SharedPrefsHelper.KEY_BUILD_DATE_LAST_ANALYSIS, -1);
