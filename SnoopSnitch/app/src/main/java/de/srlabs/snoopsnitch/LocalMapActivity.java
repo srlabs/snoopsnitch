10eae7395e4d8ce23537dc8a3390cea0e97c1a6d
Jonas Schmid <jonas@srlabs.de>
2018-04-11 18:04:56 +0200

    Added debug flag to LocalMapActivity to enable debug logs

--- SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/LocalMapActivity.java
+++ SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/LocalMapActivity.java
@@ -2,13 +2,20 @@ package de.srlabs.snoopsnitch;
 
 import de.srlabs.snoopsnitch.R;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class LocalMapActivity extends BaseActivity {
+    private static final String mTAG = "LocalMapActivity";
+    private final boolean DEBUG = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +39,35 @@ public class LocalMapActivity extends BaseActivity {
                 public void onPageFinished(WebView view, String url) {
                     super.onPageFinished(webview, url);
                     Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_SHORT).show();
-                    Log.d("LocalMapActivity","Loaded map successfully!");
+                    Log.d(mTAG,"Loaded map successfully!");
                 }
 
                 @Override
                 public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                     Toast.makeText(getApplicationContext(), "Error while loading map: " + description, Toast.LENGTH_SHORT).show();
-                    Log.e("LocalMapActivity","Error while loading map: "+errorCode+": "+description);
+                    Log.e(mTAG,"Error while loading map: "+errorCode+": "+description);
+                }
+
+                @Override
+                public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse){
+                    Log.e(mTAG,"HTTP error while loading map: request: "+request.toString()+" response:"+errorResponse);
+                }
+
+                @Override
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
+                    Log.e(mTAG,"SSL error while loading map: error:"+error.toString());
+                }
+
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon){
+                    if(DEBUG)
+                        Log.d(mTAG,"onPageStarted: "+url);
+                }
+
+                @Override
+                public void onLoadResource(WebView view, String url){
+                    if(DEBUG)
+                        Log.d(mTAG,"onLoadResource: "+url);
                 }
             });
             webview.loadData(customHtml, "text/html", "UTF-8");
