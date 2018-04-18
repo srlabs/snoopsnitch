c8b0a555157fa9ea49b71325ff6432b708807668
Jonas Schmid <jonas@srlabs.de>
2018-04-18 16:27:57 +0200

    Added proper handling for home/up menu event in PatchalyzerMainActivity again

--- SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/PAAppFlavorSNSN.java
+++ SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/PAAppFlavorSNSN.java
@@ -2,6 +2,8 @@ package de.srlabs.snoopsnitch;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
 import de.srlabs.patchalyzer.AppFlavor;
@@ -58,8 +60,18 @@ public class PAAppFlavorSNSN implements AppFlavor {
 
     @Override
     public void homeUpButtonMainActivitiyCallback(Activity activity, MenuItem item) {
-        if(item != null && item.getItemId() == android.R.id.home)
-            activity.finish(); //TODO put old code from PatchalyzerMainActivity here
+        if(item != null && item.getItemId() == android.R.id.home) {
+            Intent upIntent;
+            if (StartupActivity.isAppInitialized()) {
+                upIntent = NavUtils.getParentActivityIntent(activity);
+            } else {
+                // StartupActivity needs to run before we can start DashboardActivity
+                upIntent = new Intent(activity, StartupActivity.class);
+            }
+            upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            activity.startActivity(upIntent);
+            activity.finish();
+        }
     }
 
     @Override
