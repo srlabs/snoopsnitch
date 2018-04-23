91496a33b9016204f81e6608a7eacc36514e8d9a
Lukas <lukas@srlabs.de>
2018-04-23 12:16:17 +0200

    [THIS WILL NOT BUILD] Adjusted PAAppFlavorSNSN

--- SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/PAAppFlavorSNSN.java
+++ SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/PAAppFlavorSNSN.java
@@ -11,7 +11,7 @@ import de.srlabs.patchalyzer.PatchalyzerMainActivity;
 import de.srlabs.snoopsnitch.util.MsdConfig;
 import de.srlabs.snoopsnitch.util.Utils;
 
-public class PAAppFlavorSNSN implements AppFlavor {
+public class PAAppFlavorSNSN extends AppFlavor {
 
     public static final String BINARIES_PATH = "/data/data/de.srlabs.snoopsnitch/lib/";
     private static final String PACKAGE_NAME = "de.srlabs.snoopsnitch";
@@ -75,104 +75,8 @@ public class PAAppFlavorSNSN implements AppFlavor {
     }
 
     @Override
-    public String getString(Context activity, String key) {
-        return (String) activity.getResources().getString(activity.getResources().getIdentifier(key, "string", PACKAGE_NAME));
+    public String getPatchalyzerActivityLabel() {
+        return "Android patch level analysis";
     }
-
-    @Override
-    public int getShowInconclusiveMenuItemID() {
-        return R.id.menu_action_pa_inconclusive;
-    }
-
-    @Override
-    public int getPatchAnalysisMenuId() {
-        return R.menu.patch_analysis;
-    }
-
-    @Override
-    public int getPatchalyzerLogoId() {
-        return R.drawable.ic_patchalyzer;
-    }
-
-    @Override
-    public int getPatchalyzerLayoutId() {
-        return R.layout.activity_patchalyzer;
-    }
-
-    @Override
-    public int getCustomButtonLayoutId() {
-        return R.layout.custom_button;
-    }
-
-    @Override
-    public int getTestButtonId() {
-        return R.id.btnDoIt;
-    }
-
-    @Override
-    public int getWebViewLayoutId() {
-        return R.id.scrollViewTable;
-    }
-
-    @Override
-    public int getErrorTextId() {
-        return R.id.errorText;
-    }
-
-    @Override
-    public int getPercentagTextId() {
-        return R.id.textPercentage;
-    }
-
-    @Override
-    public int getLegendWebViewId() {
-        return R.id.legend;
-    }
-
-    @Override
-    public int getMetaInfoViewId() {
-        return R.id.scrollViewText;
-    }
-
-    @Override
-    public int getProgressBarId() {
-        return R.id.progressBar;
-    }
-
-    @Override
-    public int getResultChartId() {
-        return R.id.sumResultChart;
-    }
-
-    @Override
-    public int getProgressBoxId() {
-        return R.id.progress_box;
-    }
-
-    @Override
-    public int getColor(Context context, String colorId) {
-        return context.getResources().getColor(context.getResources().getIdentifier(colorId, "color", PACKAGE_NAME));
-
-    }
-
-    @Override
-    public int[] getStyleIdResultChart() {
-        return R.styleable.PatchalyzerSumResultChart;
-    }
-
-    @Override
-    public int getStyleResultChart(String styleableId) {
-        switch(styleableId){
-            case "PatchalyzerSumResultChart_shownumbers":
-                return R.styleable.PatchalyzerSumResultChart_shownumbers;
-            case "PatchalyzerSumResultChart_small":
-                return R.styleable.PatchalyzerSumResultChart_small;
-            case "PatchalyzerSumResultChart_drawborder":
-                return R.styleable.PatchalyzerSumResultChart_drawborder;
-            default:
-                return -1;
-        }
-    }
-
 
 }
