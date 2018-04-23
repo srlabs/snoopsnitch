1c8bd217df3b31d306c99712a6c05adadaff3e71
Lukas <lukas@srlabs.de>
2018-04-23 12:02:51 +0200

    [THIS WILL NOT BUILD] Removed patchalyzer files which are going to be included via a git submodule.

--- SnoopSnitch/app/src/main/java/de/srlabs/patchalyzer/analysis/java_basic_tests/JavaBasicTest.java
+++
@@ -1,8 +0,0 @@
-package de.srlabs.patchalyzer.analysis.java_basic_tests;
-
-
-import android.content.Context;
-
-public interface JavaBasicTest {
-    public Boolean runTest(Context c) throws Exception;
-}
---
+++ SnoopSnitch/app/src/main/java/de/srlabs/snoopsnitch/App.java
@@ -0,0 +1,14 @@
+package de.srlabs.snoopsnitch;
+
+import android.app.Application;
+import android.util.Log;
+
+import de.srlabs.patchalyzer_module.Constants;
+
+public class App extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+        Log.d(Constants.LOG_TAG,"app run...");
+        AppFlavor.setAppFlavor(new AppFlavorStandalone());
+    }
+}
